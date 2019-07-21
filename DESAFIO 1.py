14f201ff1e4b898f64f68d28658d630aef1d004b
Rodrickhd <50782890+Rodrickhd@users.noreply.github.com>
2019-07-21 13:23:37 -0300

    Add files via upload

---
+++ DESAFIO 1.py
@@ -0,0 +1,16 @@
+dia = input ('dia')
+mes = input ('mes')
+ano = input('ano')
+print ('Você Nasceu no dia',dia,'de',mes,'de',ano,'Correto?')
+
+num=int(input("Digite um numero:"))
+numero=int(input("Digite outro numero"))
+som=input(sum([num,numero]))
+print("A soma desses dois numeros é",sum)
+
+num=int(input("Digite um numero:"))
+numero=int(input("Digite outro numero"))
+som=input(sum([num,numero]))
+print("A soma desses dois numeros é",sum)
+
+
