f20d0fa1f98b81daf8f547fb8787a1e9c6af2f79
Rodrickhd <50782890+Rodrickhd@users.noreply.github.com>
2019-07-21 13:58:12 -0300

    Add files via upload

---
+++ DESAFIO 1.py
@@ -0,0 +1,17 @@
+nome = input('Qual é o seu nome?  ')
+print ('Olá',nome,'Prazer em te conhecer!')
+
+dia = input ('dia')
+mes = input ('mes')
+ano = input('ano')
+print ('Você Nasceu no dia',dia,'de',mes,'de',ano,'Correto?')
+
+num1=int(input("Digite um numero:"))
+num2=int(input("Digite outro numero"))
+soma=num1 + num2
+print("A soma desses dois numeros é",soma)
+
+
+
+
+
